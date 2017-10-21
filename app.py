2409b0e9698734fead20249d6e58995ce791b4c1
jalal.khan <jalal.khan@trifecta.com>
2017-10-21 12:25:14 -0400

    added projects route

--- app.py
+++ app.py
@@ -30,22 +30,45 @@ user = auth.sign_in_with_email_and_password('jmankhan1@gmail.com', 'password')
 
 db = firebase.database()
 
-@app.route("/projects")
-def getProject():
+class ProjectException(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.route("/projects/", methods=["GET", "POST"])
+def handleRoute():
+	if request.method == "POST":
+		return insertProject(request)
+	elif request.method == "GET":
+		return getProject(request)
+	else:
+		raise ProjectException("There was an error processing your request" , status_code=400)
+
+def getProject(request):
 	"""
 		Gets a project by id, if provided
 		Otherwise returns all project ids
 
-		TODO: better error handling
+		TODO: return as array!
 	"""
 	id = request.args.get('id')
 	
-	projects = db.child('projects').child(id).get(user['idToken']) if id != None else db.child('projects').get(user['idToken'])
+	projects = db.child('projects').child(id).get(user['idToken']) if id != None else db.child('projects').order_by_key().get(user['idToken'])
 
 	return jsonify(projects.val())
 
-@app.route("/projects/", methods=["POST"])
-def insertProject():
+def insertProject(request):
 	"""
 		Inserts a new project with a title attribtue
 		Use the following curl command to test:
@@ -53,15 +76,24 @@ def insertProject():
 
 		TODO: better error handling!
 	"""
-	project_json = request.get_json()
-
+	data = request.get_json()
+	id = db.generate_key()
+	data['id'] = id
+	print(data)
 	try:
-		db.child("projects").child(project_json['id']).set(project_json['title'], user['idToken'])
+		db.child("projects").push(data, user['idToken'])
 	except:
-		return "Something went wrong inserting the project"
-	
-	return "Success"
+		return jsonify('{ status : "error", message : "Something went wrong inserting the project" }')
+	print('returning success')
+	return jsonify('{ status : "Success" }')
+
+@app.errorhandler(ProjectException)
+def err(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', port=5001, debug=True)
 
